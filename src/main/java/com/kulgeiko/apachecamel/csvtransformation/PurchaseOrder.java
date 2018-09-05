21ad4a3575afe7c40b6d3a8008173fac281c3da9
Oleksandr Kulgeiko <akulgeiko@gmail.com>
2018-09-05 10:31:12 +0300

    added Camel csv transformation

---
+++ src/main/java/com/kulgeiko/apachecamel/csvtransformation/PurchaseOrder.java
@@ -0,0 +1,47 @@
+package com.kulgeiko.apachecamel.csvtransformation;
+
+import java.math.BigDecimal;
+
+import java.math.BigDecimal;
+import org.apache.camel.dataformat.bindy.annotation.CsvRecord;
+import org.apache.camel.dataformat.bindy.annotation.DataField;
+
+/**
+ * Created by akulgeiko on 9/4/2018.
+ */
+@CsvRecord(separator = ",", crlf = "UNIX")
+public class PurchaseOrder {
+
+    @DataField(pos = 1)
+    private String name;
+
+    @DataField(pos = 2, precision = 2)
+    private BigDecimal price;
+
+    @DataField(pos = 3)
+    private int amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/com/kulgeiko/apachecamel/csvtransformation/PurchaseOrderBindyTest.java
@@ -0,0 +1,57 @@
+package com.kulgeiko.apachecamel.csvtransformation;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.dataformat.BindyType;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by akulgeiko on 9/4/2018.
+ */
+public class PurchaseOrderBindyTest {
+
+    public static void main(String args[]) throws Exception {
+
+        // Camel context
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(createRoute());
+        context.start();
+
+        // Orders
+        PurchaseOrder order1 = new PurchaseOrder();
+        order1.setAmount(1);
+        order1.setPrice(new BigDecimal("49.95"));
+        order1.setName("Camel in Action");
+
+        PurchaseOrder order2 = new PurchaseOrder();
+        order2.setAmount(2);
+        order2.setPrice(new BigDecimal("60.98"));
+        order2.setName("Scala");
+
+        List orders = new ArrayList<PurchaseOrder>();
+        orders.add(order1);
+        orders.add(order2);
+
+        // Push to endpoint
+        ProducerTemplate template = context.createProducerTemplate();
+        template.sendBody("direct:toCsv", orders);
+
+    }
+
+    public static RouteBuilder createRoute() {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                from("direct:toCsv")
+                        .marshal().bindy(BindyType.Csv, "com.kulgeiko.apachecamel.csvtransformation")
+                        .to("file://target/orders/received?fileName=report-${header.Date}.csv");
+            }
+        };
+    }
+}
+
